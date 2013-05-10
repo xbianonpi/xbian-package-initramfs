6da6c05453e05ee31de4dc3db1323ca76cfd89e3
Koenkk <koenkanters94@gmail.com>
2013-05-10 20:26:14 +0200

    Removed "-" between packagename and version

--- gen.package.sh
+++ gen.package.sh
@@ -4,4 +4,4 @@ package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
 fakeroot find ./content  | grep -v DEBIAN/ | xargs md5sum > ./content/DEBIAN/md5sums 
-fakeroot dpkg-deb -b ./content "${package}"-"${version}".deb
+fakeroot dpkg-deb -b ./content "${package}""${version}".deb

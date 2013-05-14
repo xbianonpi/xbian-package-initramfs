35eb735d51b48dc1fb05d29e30307da8b1b8847d
Matus Kral <matuskral@me.com>
2013-05-14 18:31:25 +0200

    sorted hash file

--- gen.package.sh
+++ gen.package.sh
@@ -3,5 +3,5 @@
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
-fakeroot find ./content  | grep -v DEBIAN/ | xargs md5sum > ./content/DEBIAN/md5sums 
+fakeroot find ./content  | grep -v DEBIAN/ | sort | xargs md5sum > ./content/DEBIAN/md5sums 
 fakeroot dpkg-deb -b ./content "${package}""${version}".deb

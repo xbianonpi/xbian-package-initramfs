b039a8f7f38396827238fa99c22026b459c32e87
Matus Kral <matuskral@me.com>
2013-05-22 00:23:42 +0200

    control without size info, see comments in gen.package.sh

--- gen.package.sh
+++ gen.package.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+rm_size() {
+	cat ./content/DEBIAN/control | grep -v "Installed-Size:" > ./content/DEBIAN/control.new
+	mv ./content/DEBIAN/control.new ./content/DEBIAN/control
+}
+
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
-cat ./content/DEBIAN/control | grep -v "Installed-Size:" > ./content/DEBIAN/control.new
-mv ./content/DEBIAN/control.new ./content/DEBIAN/control
+# calculate size dynamically. remove first any entry, then add the actual 
+rm_size
 printf "Installed-Size: %d\n" $(du -s ./content | awk '{print $1}') >> ./content/DEBIAN/control
 
-fakeroot find ./content  | grep -v DEBIAN/ | sort | xargs md5sum > ./content/DEBIAN/md5sums 
+cd content
+find ./ -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P\0' | sort -z| xargs --null md5sum > DEBIAN/md5sums
+cd ..
 fakeroot dpkg-deb -b ./content "${package}""${version}".deb
+
+# remove the size again, because on different filesystems du will return different size
+rm_size

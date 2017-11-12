30cff00725dbfaac0b758ddc5b11c088cc3d4a66
mkreisl <manfred.kreisl@nexgo.de>
2017-11-12 14:42:41 +0100

    Unifying initram.switcher.sh code

--- content/etc/xbian-initramfs/initram.switcher.sh
+++ content/etc/xbian-initramfs/initram.switcher.sh
@@ -1,52 +1,62 @@
 #!/bin/bash
 
 mountpoint -q /boot || exit 0
+
 ramfs=yes
 
 [ ! -e /etc/default/xbian-initramfs ] || . /etc/default/xbian-initramfs
 
-ramfs_required() {
+function ramfs_check() {
+    bootfile=$1
+    { z=$(grep root= $bootfile); z=$(echo ${z##* root=} | awk '{print $1}'); }
+
+    case $z in
+
+        UUID=*|LABEL=*)
+            root=$(findfs $(echo $z|tr -d '"') 2>/dev/null)
+            case $root in
+                /dev/mmcblk*|/dev/sd*)
+	            sed -i "s%root=$z%root=$root%" $bootfile
+	            z=$root
+	            ramfs=no
+                ;;
+                *)
+                ;;
+            esac
+        ;;
+
+        PARTUUID=*)
+            root=$(findfs $(echo $z|tr -d '"') 2>/dev/null)
+            case $root in
+                /dev/mmcblk0*|/dev/sd*)
+	            z=$root
+	            ramfs=no
+                ;;
+                *)
+                ;;
+            esac
+        ;;
+
+        ZFS=*)
+            z=${z##ZFS=}
+        ;;
+
+        /dev/mmcblk*|/dev/sd*|/dev/nfs)
+            grep -q "vers=4\|rootfstype=f2fs" $bootfile || ramfs=no
+        ;;
+
+        *)
+        ;;
+    esac
+
     { [ -e /var/run/reboot-required ] || [ "$FORCEINITRAM" = yes ] || grep -wq 'bootmenu\|rescue' /boot/cmdline.txt 2>/dev/null; } && ramfs=yes || :
     [ "$FORCEINITRAM" != disabled ] || ramfs=no
-    echo "$ramfs"
 }
 
 case $(xbian-arch) in
     RPI)
-        { z=$(cat /boot/cmdline.txt); z=$(echo ${z##* root=} | awk '{print $1}'); }
-
-        case $z in
-            UUID=*|LABEL=*)
-	        root=$(findfs $(echo $z|tr -d '"') 2>/dev/null)
-	        case $root in
-	            /dev/mmcblk0*|/dev/sd*)
-		        sed -i "s%root=$z%root=$root%" /boot/cmdline.txt
-		        z=$root
-		        ramfs=no
-                    ;;
-	            *)
-                    ;;
-	        esac
-            ;;
-            PARTUUID=*)
-	        root=$(findfs $(echo $z|tr -d '"') 2>/dev/null)
-	        case $root in
-	            /dev/mmcblk0*|/dev/sd*)
-		        z=$root
-		        ramfs=no
-                    ;;
-	            *)
-                    ;;
-	        esac
-            ;;
-            /dev/mmcblk0*|/dev/sd*|/dev/nfs)
-	        grep -q "vers=4\|rootfstype=f2fs" /boot/cmdline.txt || ramfs=no
-            ;;
-            *)
-            ;;
-        esac
-
-        case $(ramfs_required) in
+        ramfs_check /boot/cmdline.txt
+        case $ramfs in
             yes)
                 grep -q "^#initramfs " /boot/config.txt && sed -i 's/^#initramfs /initramfs /' /boot/config.txt
             ;;
@@ -57,26 +67,8 @@ case $(xbian-arch) in
     ;;
 
     iMX6)
-        { z=$(grep root= /boot/boot.scr.txt); z=$(echo ${z##* root=} | awk '{print $1}'); }
-
-        case $z in
-            ZFS=*)
-                z=${z##ZFS=}
-                ramfs=yes
-            ;;
-            UUID=*|LABEL=*)
-                z=$(findfs $(echo $z|tr -d '"') 2>/dev/null)
-                ramfs=yes
-            ;;
-            /dev/mmcblk0*|/dev/nfs)
-                grep -q "vers=4\|rootfstype=f2fs" /boot/boot.scr.txt && ramfs=yes || ramfs=no
-            ;;
-            *)
-                ramfs=no
-            ;;
-        esac
-
-        case $(ramfs_required) in
+        ramfs_check /boot/boot.scr.txt
+        case $ramfs in
             yes)
                 [ -e /boot/initramfs.gz.notinuse -a ! -e /boot/initramfs.gz ] && mv /boot/initramfs.gz.notinuse /boot/initramfs.gz
             ;;
@@ -90,12 +82,12 @@ case $(xbian-arch) in
     ;;
 esac
 
-if grep -q ip= /boot/cmdline.txt 2>/dev/null || grep -q ip= /boot/boot.scr.txt 2>/dev/null; then
+if grep -q ip= $bootfile 2>/dev/null; then
     if [ $z = $(findmnt -r -n -v -o SOURCE /) ] && grep -q 'iface eth0 inet dhcp' /etc/network/interfaces; then
         sed -i 's%iface eth0 inet dhcp%iface eth0 inet manual%' /etc/network/interfaces
         touch /etc/xbian-initramfs/eth0.swap.eth0
     fi
-elif ! grep -q ip= /boot/cmdline.txt 2>/dev/null && ! grep -q ip= /boot/boot.scr.txt 2>/dev/null; then
+elif ! grep -q ip= $bootfile 2>/dev/null; then
     if [ $z = $(findmnt -r -n -v -o SOURCE /) -a -e /etc/xbian-initramfs/eth0.swap.eth0 ]; then
         sed -i 's%iface eth0 inet manual%iface eth0 inet dhcp%' /etc/network/interfaces
         rm -f /etc/xbian-initramfs/eth0.swap.eth0

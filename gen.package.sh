2fd7a02a164db59238f47c3f19a92abbcf0b5d8e
mkreisl <manfred.kreisl@nexgo.de>
2023-11-14 18:59:11 +0100

    Add the Architecture to the file name

--- gen.package.sh
+++ gen.package.sh
@@ -18,11 +18,12 @@ fi
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version1=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}' | awk -F'-' '{print $1}')
 version2=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}' | awk -F'-' '{print $2}')
+arch="_$(grep -m1 'Architecture\:' ./content-tpl/DEBIAN/control | awk -F':' '{print $2}' | tr -d ' ')"
 
-[ $(date +%Y%m%d) = $version1 ] && version2=$((version2 + 1)) || version2=0
+[ $(date +%Y%m%d) = $version1 ] && version2="-$((version2 + 1))" || version2=''
 version1=$(date +%Y%m%d)
 
-version=$version1-$version2
+version=$version1$version2
 for f in $(find ./content-tpl -type f -printf "%P\n"); do
     cp ./content-tpl/$f content/$f
     sed -i "s%__DATE__%$version%g" ./content/$f
@@ -37,7 +38,7 @@ find ./ -type f -print0 | xargs -0 -L1 printf "%s\n" | while read f; do [ -z "$s
 printf "Installed-Size: %u\n" $(cat ../size.txt) >> ./DEBIAN/control
 find ./ -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P\0' | sort -z| xargs --null md5sum > DEBIAN/md5sums
 cd ..
-fakeroot dpkg-deb -b ./content "${package}""${version}".deb
+fakeroot dpkg-deb -b ./content "${package}-${version}${arch}".deb
 [ -d ./tmp ] && mv ./tmp ./content
 # remove the size again, because on different filesystems du will return different size
 rm_size
